4c9c5dfe4804c7fe22483dd737a4abf16cdadd05
Seongwon Lee <62098910+seongwonme@users.noreply.github.com>
2021-07-21 23:52:13 +0900

    Update JS Study.js

--- JS Study.js
+++ JS Study.js
@@ -1,2 +1,10 @@
+# 이진수 변환 
 let a = 3
 a.toString(2) // "11" 이진수 리턴
+
+# Input 덧셈
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' '); //모든 문자열이 들어온다.
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a+b);
